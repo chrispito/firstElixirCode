09f761d224d6d50ef5a6032a82af290542ed8c46
chrispito <tdc_86@yahoo.fr>
2018-03-05 16:07:23 +0100

    Identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,75 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
 
-  @doc """
-  Hello world.
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
+  end
 
-  ## Examples
+  def build_pixel_map(%Identicon.Image{grid: grid, color: color} = image) do
+    pixel_map = Enum.map grid, fn({code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
 
-      iex> Identicon.hello
-      :world
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
 
-  """
-  def hello do
-    :world
+      result = nil
+      if rem(code, 2) == 0 do
+        result = {top_left, bottom_right, color}
+      else 
+        result = {top_left, bottom_right, {25, 55, 158}}
+      end
+      result
+    end
+
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{pixel_map: pixel_map} = image) do
+    image = :egd.create(250, 250)
+
+    Enum.each pixel_map, fn({start, stop, color}) ->
+      :egd.filledRectangle(image, start, stop, :egd.color(color))
+    end
+
+    :egd.render(image)
+  end
+
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
